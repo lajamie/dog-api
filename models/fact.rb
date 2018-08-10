ae5ccd91d5d32dc470da1f53d265b5afaf13e096
lajamie <lajamie@nd.edu>
2018-08-09 22:06:34 -0400

    Update fact.rb

--- models/fact.rb
+++ models/fact.rb
@@ -2,7 +2,7 @@ class Fact < ActiveRecord::Base
   def self.get_random(number = 1)
     number = number.to_i if number.respond_to?(:to_i)
     number = 1 if number == 0
-    number = 100 if number > 100
+    number = 5 if number > 5
 
     order('RANDOM()').limit(number)
   end
